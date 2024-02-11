8d65af593ea04769dc6ec60db367e81951ea6460
brociolol <118844723+brociolol@users.noreply.github.com>
2024-02-11 10:12:40 -0600

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,10 @@
 const verifyRoute = require('./verify');
 const router = require('express').Router();
 
+router.get('/', (req, res) => {
+	res.render('index');
+});
+
 router.use('/verify', verifyRoute);
 
 router.get('*', function(req, res){
